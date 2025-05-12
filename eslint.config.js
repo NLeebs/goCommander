3b7dd464db844a57dc9fa5be68e9d909007fb901
NLeebs <lieberman.nathan@gmail.com>
2025-05-12 16:00:47 -0400

    Added postcss plugin and config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,21 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+  },
+  {
+    files: ["**/*.css"],
+    rules: {
+      "at-rule-no-unknown": [
+        true,
+        {
+          ignoreAtRules: ["tailwind", "apply", "layer", "screen"],
+        },
+      ],
+    },
   },
 ]);
