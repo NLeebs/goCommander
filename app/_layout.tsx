18de16bc1ef1fd0f4875c9ef30c1b8b1e1808dcc
NLeebs <lieberman.nathan@gmail.com>
2025-05-14 15:12:49 -0400

    Cleared app of example files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,11 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import "@/global.css";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { ErrorBoundary } from "react-error-boundary";
 import { Text, View } from "react-native";
 import "react-native-reanimated";
-import "../global.css";
-
-import { useColorScheme } from "@/hooks/useColorScheme";
 
 const ErrorFallback = ({ error }: { error: Error }) => (
   <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -27,7 +21,6 @@ const LoadingScreen = () => (
 );
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -38,19 +31,12 @@ export default function RootLayout() {
 
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        {/* Add your other providers here */}
-        {/* <AuthProvider> */}
-        {/* <StoreProvider> */}
-        {/* <ApiProvider> */}
+      <ThemeProvider value={DefaultTheme}>
         <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="HomeScreen" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" />
-        {/* </ApiProvider> */}
-        {/* </StoreProvider> */}
-        {/* </AuthProvider> */}
       </ThemeProvider>
     </ErrorBoundary>
   );
